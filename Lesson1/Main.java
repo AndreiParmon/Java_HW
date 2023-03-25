fee5e131ec5f20d35382b66e7a645ef9ddc5bb19
AndreiParmon <118477491+AndreiParmon@users.noreply.github.com>
2023-03-25 14:55:04 +0300

    add Main

---
+++ Lesson1/Main.java
@@ -0,0 +1,32 @@
+package Lesson1.homeWork;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // для Task1
+//        Scanner iScanner = new Scanner(System.in);
+//        System.out.printf("Введите N: ");
+//        int count = iScanner.nextInt();
+//        Task1.triangularNumberCalculation(count);
+
+        // для Task2
+//        Scanner iScanner = new Scanner(System.in);
+//        System.out.printf("Введите N: ");
+//        int count = iScanner.nextInt();
+//        Task2.factorialCalculation(count);
+
+        // для Task3
+//        Task3.outputOfPrimeNumbers();
+
+        // для Task4
+//        String operation;
+//        Scanner iScanner = new Scanner(System.in);
+//        System.out.printf("Введите первое число: ");
+//        int number1 = iScanner.nextInt();
+//        System.out.printf("Введите второе число: ");
+//        int number2 = iScanner.nextInt();
+//        System.out.printf("Какую операцию хотите выполнить? ");
+//        operation = iScanner.next();
+//        Task4.calculator(operation, number1, number2);
+    }
+}
