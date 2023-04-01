2466eca78b4131682432e80c64a2a3f41e87dc54
AndreiParmon <118477491+AndreiParmon@users.noreply.github.com>
2023-04-01 12:52:33 +0300

    add main

---
+++ Lesson2/Main.java
@@ -0,0 +1,14 @@
+package lesson2.homeWork;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+//        для Task1
+        int[] array = new int[] {85,8,2,6,7,9,4,3};
+        System.out.println(Arrays.toString(Task1.bubbleSort(array)));
+
+//        для Task2
+        Task2.textDivision();
+    }
+}
