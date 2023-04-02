79efb8bc290ac6761085fe9189d22b1645a3c3dc
AndreiParmon <118477491+AndreiParmon@users.noreply.github.com>
2023-04-02 14:35:53 +0300

    add main

---
+++ Lesson3/Main.java
@@ -0,0 +1,14 @@
+package lesson3.homeWork;
+
+public class Main {
+    public static void main(String[] args) {
+//        для Task2
+        Task2.removingNumbers();
+
+//        для Task3
+        Task3.minMaxAverage();
+
+//        для Task4
+        Task4.differenceSymmetryArrays();
+    }
+}
