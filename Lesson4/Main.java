b90778ba242d663db465cee42615e208ea8ef2cc
AndreiParmon <118477491+AndreiParmon@users.noreply.github.com>
2023-04-05 20:36:17 +0300

    add main

---
+++ Lesson4/Main.java
@@ -0,0 +1,11 @@
+package lesson4.homeWork;
+
+public class Main {
+    public static void main(String[] args) {
+//        для Task1
+//        Task1.flippedList();
+
+//        для Task2
+//        Task2.queueMethods();
+    }
+}
